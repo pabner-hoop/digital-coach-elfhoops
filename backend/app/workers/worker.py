98c69050bfa2244697e22b78fba1cb001b6e264b
Paul Abner <pabner@tractian.com>
2025-12-30 03:06:52 -0300

    feat: add all functions upload videos

--- backend/app/workers/worker.py
+++ backend/app/workers/worker.py
@@ -1,14 +1,14 @@
 import os
 from redis import Redis
-from rq import Worker, Queue, Connection
+from rq import Worker, Queue
 
 def main():
     redis_url = os.getenv("REDIS_URL", "redis://localhost:6379/0")
     conn = Redis.from_url(redis_url)
 
-    with Connection(conn):
-        worker = Worker([Queue("default")])
-        worker.work(with_scheduler=False)
+    q = Queue("default", connection=conn)
+    worker = Worker([q], connection=conn)
+    worker.work(with_scheduler=False)
 
 if __name__ == "__main__":
     main()
